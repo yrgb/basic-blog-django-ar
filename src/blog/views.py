2280cf35ce23c159184bc343ef3fde963204324d
yusuf <yousefrageb@gmail.com>
2021-04-03 16:31:54 +0300

    create template for home and about pages

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+
+
+    context = {
+        'title': 'الصفحة الرئيسية',
+    }
+    return render(request, 'blog/index.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'من أنا'})
